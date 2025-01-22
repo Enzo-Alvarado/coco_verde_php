2d915d02264c5d219d89cf759b71b4d471f9a4e8
Enzo Alvarado <alvaradoenzo4@gmail.com>
2025-01-22 02:54:51 -0300

    commit base de datos configurada

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php 
 
 function conectarDB() : mysqli {
-    $db = mysqli_connect('localhost', 'root', 'Enzo45458753', 'coco_verde');
+    $db = mysqli_connect('127.0.0.1:3306', 'u317872803_ComCode', 'CocoVerde2025', 'u317872803_coco_verde');
 
     if(!$db) {
         echo 'Error de conexión';
